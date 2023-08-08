c1c05416f8e772c1d547a7658103f0610c6f43a2
0xJordan <ahmedjamlan12@gmail.com>
2023-08-08 16:12:54 +0300

    Added 2 exercises

---
+++ src/intro-01/intro_01.ts
@@ -0,0 +1,5 @@
+let user = {name: '0xjordan', age: 22}
+
+console.log("0xjordan");
+console.log(user.name);
+
---
+++ src/intro-01/variables.ts
@@ -0,0 +1,5 @@
+let greetings: string = "Hello there!"
+
+greetings.toLowerCase();
+
+console.log(greetings);
