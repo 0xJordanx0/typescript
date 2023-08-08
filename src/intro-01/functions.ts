186036818dc43f830d309674a661e96389ebc1cc
0xJordan <ahmedjamlan12@gmail.com>
2023-08-08 16:40:04 +0300

    Uploaded functions exercise

---
+++ src/intro-01/functions.ts
@@ -0,0 +1,22 @@
+function addTwo(num: number): number{
+    return num + 2
+}
+
+function getUpper(val: string){
+    return val.toUpperCase();
+}
+
+function signUpUser(name: string, email: string, password:string){
+    // Create user
+}
+
+let loginUser = (name:string , email:string , isAuth:boolean = false) => {
+
+}
+
+let myValue = addTwo(5);
+
+getUpper("0xjordan");
+signUpUser("0xjordan","test@email.com","123456789");
+loginUser("0xjordan", "test@gmail.com")
+
---
+++ src/intro-01/types.ts
@@ -0,0 +1,10 @@
+let hero: string;
+let userId: number;
+let userAmount: number;
+let userExists: boolean;
+
+
+hero = "Superman";
+userId = 3445;
+userAmount = 3000;
+userExists = true;
