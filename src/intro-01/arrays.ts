544e8ae63f08ed8ef0eb30050d4207df6fcd4181
0xJordan <ahmedjamlan12@gmail.com>
2023-08-09 08:10:04 +0300

    Added Arrays

---
+++ src/intro-01/arrays.ts
@@ -0,0 +1,15 @@
+const superHeros: string[] = []
+//const heroPower: number[] = []
+const heroPower: Array<number> = []
+
+type UserDefault = {
+    name: string,
+    isActive: boolean,
+}
+
+const allUsers: UserDefault[] = [];
+
+superHeros.push("spiderman");
+heroPower.push(2);
+
+allUsers.push({name: "test", isActive: true});
